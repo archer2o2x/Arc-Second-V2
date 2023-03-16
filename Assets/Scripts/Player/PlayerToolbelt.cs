c3299abc44402393138687e46898508f70aa9e53
archer2o2x <31862296+archer2o2x@users.noreply.github.com>
2023-03-16 12:23:20 +0000

    still working on item pickup logic

--- Assets/Scripts/Player/PlayerToolbelt.cs
+++ Assets/Scripts/Player/PlayerToolbelt.cs
@@ -6,10 +6,17 @@ public class PlayerToolbelt : MonoBehaviour
 {
     public Dictionary<string, int> Inventory;
 
+    private void Start()
+    {
+        Inventory = new Dictionary<string, int>();
+    }
+
     public void TryPickupItem(Item item)
     {
         if (Inventory.ContainsKey(item.ItemID)) Inventory[item.ItemID] = Inventory[item.ItemID] + item.ItemAmount;
         else Inventory[item.ItemID] = item.ItemAmount;
+
+        Destroy(item.gameObject);
     }
 
     public bool CanUseItem(string ItemID, int amount)
@@ -28,5 +35,14 @@ public class PlayerToolbelt : MonoBehaviour
         Inventory[ItemID] -= amount;
 
         return true;
+    }
+
+    [ContextMenu("List Inventory Contents")]
+    public void DisplayContents()
+    {
+        foreach (var key in Inventory.Keys)
+        {
+            Debug.Log("    " + Inventory[key].ToString().PadRight(4) + " X " + key);
+        }
     }
 }
